714162c0874223a3b74f4ea86f5b4a8bc099ba19
KK <morrkade05@gmail.com>
2021-08-26 19:18:19 -0500

    Updated to match new phasmo update

--- index.js
+++ index.js
@@ -1,14 +1,7 @@
 var shy = true;
 var showTips = false;
 
-var evidenceSelections = {
-  box: 0,
-  fingers: 0,
-  writing: 0,
-  freezing: 0,
-  orb: 0,
-  emf: 0,
-};
+var evidenceSelections = {};
 
 var evidenceTypes = {
   box: 'Spirit Box',
@@ -17,85 +10,104 @@ var evidenceTypes = {
   freezing: 'Freezing Temperatures',
   orb: 'Ghost Orbs',
   emf: 'EMF Level 5',
-  types: ['box', 'fingers', 'writing', 'freezing', 'orb', 'emf'],
+  dots: 'DOTS Projector',
+  types: ['dots', 'emf', 'fingers', 'freezing', 'orb', 'writing', 'box'],
 };
 
+addEvidenceSelection();
+
 const ghosts = {
-  spirit: {
-    evidence: ['box', 'fingers', 'writing'],
-    strengths: 'Nothing',
-    weaknesses: 'Smudge Sticks',
-    other: 'Footstep sound timer is 2-15s where as other ghosts have a timer of 15-45s',
-  },
-  wraith: {
-    evidence: ['fingers', 'freezing', 'box'],
-    strengths: 'Leaves no footprints (salt)',
-    weaknesses: 'Salt',
-  },
-  phantom: {
-    evidence: ['emf', 'orb', 'freezing'],
-    strengths: 'Looking drops sanity',
-    weaknesses: 'Taking photo makes it disappear temporarily',
-    other: 'When viewing ghost, it will flash in and out of view in 1s+ intervals',
-  },
-  poltergeist: {
-    evidence: ['box', 'fingers', 'orb'],
-    strengths: 'Can throw lots of objects at once',
-    weaknesses: 'Ineffective in an empty room',
-  },
   banshee: {
-    evidence: ['emf', 'fingers', 'freezing'],
+    evidence: ['dots', 'fingers', 'orb'],
     strengths: 'Targets one person at a time',
     weaknesses: 'Less effective near a crucifix',
     other:
       'If you can find who its targeting, make that person hide while the rest of the team gathers evidence',
   },
+  demon: {
+    evidence: ['fingers', 'freezing', 'writing'],
+    strengths: 'Will attack more than other ghost types',
+    weaknesses: 'Asking successful questions on Ouija Board wont lower users sanity',
+  },
+  goryo: {
+    evidence: ['dots', 'emf', 'fingers'],
+    strengths: 'Only show itself on camera if no people are nearby',
+    weaknesses: 'Rarely seen far from their place of death',
+  },
+  hantu: {
+    evidence: ['fingers', 'freezing', 'orb'],
+    strengths: 'Faster in cold areas',
+    weaknesses: 'Slow in warmer areas',
+  },
   jinn: {
-    evidence: ['box', 'orb', 'emf'],
+    evidence: ['emf', 'fingers', 'freezing'],
     strengths: 'Travels faster if current victim is far away',
     weaknesses: 'Turning off breaker will disable its ability',
   },
   mare: {
-    evidence: ['box', 'orb', 'freezing'],
+    evidence: ['orb', 'writing', 'box'],
     strengths: 'Increased chance of attacking in the dark',
     weaknesses: 'Turning on lights lowers attack chance',
   },
-  revenant: {
+  myling: {
     evidence: ['emf', 'fingers', 'writing'],
+    strengths: 'Known to be quieter when hunting',
+    weaknesses: 'Frequently make paranormal sounds',
+  },
+  oni: {
+    evidence: ['dots', 'emf', 'freezing'],
+    strengths: 'Can move objects at great speed and are more active when people are near',
+    weaknesses: 'Being more active makes it easier to find and identify',
+  },
+  phantom: {
+    evidence: ['dots', 'fingers', 'box'],
+    strengths: 'Looking drops sanity',
+    weaknesses: 'Taking photo makes it disappear temporarily',
+    other: 'When viewing ghost, it will flash in and out of view in 1s+ intervals',
+  },
+  poltergeist: {
+    evidence: ['fingers', 'writing', 'box'],
+    strengths: 'Can throw lots of objects at once',
+    weaknesses: 'Ineffective in an empty room',
+  },
+  revenant: {
+    evidence: ['freezing', 'orb', 'writing'],
     strengths: 'Travels faster when hunting a victim',
     weaknesses: 'Hiding will cause it to move slowly',
   },
   shade: {
-    evidence: ['emf', 'orb', 'writing'],
+    evidence: ['emf', 'freezing', 'writing'],
     strengths: 'Shy, and harder to find',
     weaknesses: 'Will not enter hunt mode if multiple people are near',
   },
-  demon: {
-    evidence: ['box', 'writing', 'freezing'],
-    strengths: 'Will attack more than other ghost types',
-    weaknesses: 'Asking successful questions on Ouija Board wont lower users sanity',
+  spirit: {
+    evidence: ['emf', 'writing', 'box'],
+    strengths: 'Nothing',
+    weaknesses: 'Smudge Sticks',
+    other: 'Footstep sound timer is 2-15s where as other ghosts have a timer of 15-45s',
+  },
+  wraith: {
+    evidence: ['dots', 'emf', 'box'],
+    strengths: 'Leaves no footprints (salt)',
+    weaknesses: 'Salt',
   },
   yurei: {
-    evidence: ['orb', 'writing', 'freezing'],
+    evidence: ['dots', 'freezing', 'orb'],
     strengths: 'Stronger effect on sanity',
     weaknesses: 'Smudging the room causes it to not wonder room for long',
   },
-  oni: {
-    evidence: ['emf', 'box', 'writing'],
-    strengths: 'Can move objects at great speed and are more active when people are near',
-    weaknesses: 'Being more active makes it easier to find and identify',
-  },
-  hantu: {
-    evidence: ['fingers', 'orb', 'writing'],
-    strengths: 'Faster in cold areas',
-    weaknesses: 'Slow in warmer areas',
-  },
   yokai: {
-    evidence: ['box', 'orb', 'writing'],
+    evidence: ['dots', 'orb', 'box'],
     strengths: 'Talking near it will anger it',
     weaknesses: 'While hunting, it can only hear voices close to it',
   },
 };
+
+function addEvidenceSelection() {
+  for (var i of evidenceTypes.types) {
+    evidenceSelections[i] = 0;
+  }
+}
 
 function changeShyness() {
   shy = !shy;
